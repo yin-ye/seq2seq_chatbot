3cd9bec557ba47d7bf1f1cfbecec3dbb4acc2929
yin-ye <72907381+yin-ye@users.noreply.github.com>
2021-05-22 13:01:03 +0100

    Create encoder-decoder.py

---
+++ encoder-decoder.py
@@ -0,0 +1,207 @@
+import tensorflow as tf
+tf.compat.v1.enable_eager_execution()
+tf.test.is_gpu_available()
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import concurrent.futures
+import collections
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import sys
+import math
+import time
+start_exec_time = time.time()
+
+path_to_data_train = "./dstc8-train.txt"
+path_to_data_val = "./dstc8-val-candidates.txt"
+path_to_data_test = "./dstc8-test-candidates.txt"
+predicted_references = './predicted_references' #where the predicted and reference texts go
+checkpoint_dir = './training_checkpoints'  # for training weights
+
+def unicode_to_ascii(word):
+  return ''.join(c for c in unicodedata.normalize('NFD', word)
+      if unicodedata.category(c) != 'Mn')
+
+def preprocess_sentence(sentence):
+  sentence = unicode_to_ascii(sentence.lower().strip())
+  sentence = re.sub(r"([?.!,¿])", r" \1 ", sentence)
+  sentence = re.sub(r'[" "]', " ", sentence)
+  sentence = sentence.strip()
+  # adding a start and an end token to the sentence
+  sentence = '<start> ' + sentence + ' <end>'
+  return sentence
+
+
+def create_dataset(path):
+  lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+  sentence_pairs = [[preprocess_sentence(sentence) for sentence in l.split('\t')]  for l in lines]
+  return zip(*sentence_pairs)
+
+def tokenize(lang):
+  lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+  lang_tokenizer.fit_on_texts(lang)
+  tensor = lang_tokenizer.texts_to_sequences(lang)
+  tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, padding='post')
+  return tensor, lang_tokenizer
+
+def load_dataset(path):
+  inp_lang, targ_lang = create_dataset(path)
+  input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+  target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+  return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+input_tensor_train, target_tensor_train, inp_lang, targ_lang = load_dataset(path_to_data_train)
+max_length_targ, max_length_inp = target_tensor_train.shape[1], input_tensor_train.shape[1]
+
+class Encoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    super(Encoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.enc_units = enc_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.lstm = tf.keras.layers.LSTM(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True)
+
+  def call(self, x):
+    x = self.embedding(x)
+    output, hidden_state, cell_state = self.lstm(x)
+    state = [hidden_state, cell_state]
+    # hidden_state, cell_state are the encoder states
+    return output, state
+
+  def initialize_hidden_state(self):
+    return tf.zeros((self.batch_sz, self.enc_units))
+
+  class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, query, values):
+    # query hidden state shape == (batch_size, hidden size)
+    # query_with_time_axis shape == (batch_size, 1, hidden size)
+    # values shape == (batch_size, max_len, hidden size)
+    # we are doing this to broadcast addition along the time axis to calculate the score
+    query_with_time_axis = tf.expand_dims(query, 1)
+
+    # score shape == (batch_size, max_length, 1)
+    # we get 1 at the last axis because we are applying score to self.V
+    # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+    score = self.V(tf.nn.tanh(
+        self.W1(query_with_time_axis) + self.W2(values)))
+
+    # attention_weights shape == (batch_size, max_length, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector shape after sum == (batch_size, hidden_size)
+    context_vector = attention_weights * values
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+  
+  class Decoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+    super(Decoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.dec_units = dec_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.fc = tf.keras.layers.Dense(vocab_size)
+
+    # used for attention
+    self.attention = BahdanauAttention(self.dec_units)
+
+  def call(self, x, hidden, enc_output):
+    # enc_output shape == (batch_size, max_length, hidden_size)
+    context_vector, attention_weights = self.attention(hidden, enc_output)
+
+    # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+    x = self.embedding(x)
+
+    # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+    # passing the concatenated vector to the GRU
+    output, state = self.gru(x)
+
+    # output shape == (batch_size * 1, hidden_size)
+    output = tf.reshape(output, (-1, output.shape[2]))
+
+    # output shape == (batch_size, vocab)
+    x = self.fc(output)
+
+    return x, state, attention_weights
+
+  ###TRAINING#####
+  optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+  loss_ = loss_object(real, pred)
+  mask = tf.cast(mask, dtype=loss_.dtype)
+  loss_ *= mask
+  return tf.reduce_mean(loss_)
+
+@tf.function
+def train_step(inp, targ, enc_hidden):
+  loss = 0
+
+  with tf.GradientTape() as tape:
+    enc_output, enc_hidden = encoder(inp, enc_hidden)
+
+    dec_hidden = enc_hidden
+
+    dec_input = tf.expand_dims([targ_lang.word_index['<start>']] * BATCH_SIZE, 1)
+
+    # Teacher forcing - feeding the target as the next input
+    for t in range(1, targ.shape[1]):
+      # passing enc_output to the decoder
+      predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
+
+      loss += loss_function(targ[:, t], predictions)
+
+      # using teacher forcing
+      dec_input = tf.expand_dims(targ[:, t], 1)
+
+  batch_loss = (loss / int(targ.shape[1]))
+
+  variables = encoder.trainable_variables + decoder.trainable_variables
+
+  gradients = tape.gradient(loss, variables)
+
+  optimizer.apply_gradients(zip(gradients, variables))
+
+  return batch_loss
+
+def train_model(EPOCHS):
+    for epoch in range(EPOCHS):
+        start = time.time()
+
+        enc_hidden = encoder.initialize_hidden_state()
+        total_loss = 0
+
+        for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
+            batch_loss = train_step(inp, targ, enc_hidden)
+            total_loss += batch_loss
+
+            if batch % 100 == 0:
+                print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1, batch, batch_loss.numpy()))
+
+        if (epoch + 1) % 2 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                            total_loss / steps_per_epoch))
+        print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+        
